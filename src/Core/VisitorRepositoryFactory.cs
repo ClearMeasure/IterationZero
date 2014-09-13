5f260720f89c0a358c55ddb73d2250ff1a56effe
Jeffrey Palermo <jeffrey@clear-measure.com>
2014-09-13 15:15:00 -0500

    - Prepped code for Houston TechFest release

--- src/Core/VisitorRepositoryFactory.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Core
-{
-    public class VisitorRepositoryFactory
-    {
-        public static Func<IVisitorRepository> Build = () => { throw new NullReferenceException(); };
-    }
-}
---
+++ src/UnitTests/Model/VisitorTester.cs
@@ -0,0 +1,19 @@
+﻿using Core.Model;
+using NUnit.Framework;
+using Should;
+
+namespace UnitTests.Model
+{
+    [TestFixture]
+    public class VisitorTester
+    {
+        [Test]
+        public void Should_serialize_to_full_name()
+        {
+            var visitor = new Visitor(){FirstName = "Jeffrey", LastName = "Palermo"};
+            string serializedRepresentation = visitor.ToString();
+
+            serializedRepresentation.ShouldEqual("Jeffrey Palermo");
+        }
+    }
+}
